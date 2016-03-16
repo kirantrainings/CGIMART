e005bf869a1a7f18341031b3203faf6bccdf2b16
kpaturi <kpautri@smarttuition.com>
2016-03-16 11:42:21 +0530

    Added routing

--- app/app.js
+++ app/app.js
@@ -1,8 +1,30 @@
- angular.module("DemoMart",['common.filters']);
+ angular.module("DemoMart",['common.filters','ui.router']);
 
  angular.module("DemoMart")
-        .config([function(){
+        .config(["$urlRouterProvider","$stateProvider",
+         function($urlRouterProvider,$stateProvider){
          console.log("main Modules");
+                 //define states
+             $urlRouterProvider.otherwise("/");
+             var home={
+                 url:"/",
+                 templateUrl:"app/templates/home.tpl.html",
+                 controller:"homeCtrl"
+             };
+             var login={
+                 url:'/login',
+                 templateUrl:"app/templates/login.tpl.html",
+                 controller:"loginCtrl"
+             };
+             var compare={};
+             var vehicles={};
+
+             //Register state
+             $stateProvider.state("home",home);
+             $stateProvider.state("login",login);
+             /*
+             $stateProvider.state("vehicles",vehicles);
+             $stateProvider.state("compare",compare)*/;
      }]);
 
 
